4a23549cf2e82a987befe702d49de6e4f224f864
Coolpuppy24 <pat.pvp.unturned@gmail.com>
2017-04-09 02:07:10 -0500

    Plugin Created!
    Special thanks to xdlewisdx

---
+++ BuyPermission.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using SDG.Unturned;
+using Rocket.Core.Plugins;
+using Rocket.Core.Logging;
+using Rocket.Unturned.Chat;
+using Rocket.Core;
+using System.Collections.Generic;
+
+namespace coolpuppy24.buypermission
+{
+    public class BuyPermission : RocketPlugin<BuyPermissionConfiguration>
+    {
+        public int lastindex = 0;
+        public DateTime? lastmessage = null;
+
+        protected override void Load()
+        {
+            Rocket.Core.Logging.Logger.LogWarning("BuyPermission made by: Coolpuppy24 and xdlewisdx.");
+            Rocket.Core.Logging.Logger.LogWarning("Plugin Version: 1.0");
+            Rocket.Core.Logging.Logger.Log("BuyPermission Successfully Loaded!");
+        }
+
+        protected override void Unload()
+        {
+            Rocket.Core.Logging.Logger.Log("Unload");
+        }
+    }
+}
---
+++ BuyPermissionConfig.cs
@@ -0,0 +1,31 @@
+﻿using Rocket.API;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System;
+
+namespace coolpuppy24.buypermission
+{
+    public sealed class Command
+    {
+        public string PermsGroupID;
+        public string Cost;
+    }
+
+    public class BuyPermissionConfiguration : IRocketPluginConfiguration
+    {
+
+        [XmlArrayItem("BuyPerms")]
+        [XmlArray(ElementName = "BuyPermission")]
+        public List<Command> BuyPermissionCommand;
+
+        public void LoadDefaults()
+        {
+            BuyPermissionCommand = new List<Command>(){
+                new Command(){PermsGroupID="SniperKit",Cost="100"
+                },
+                new Command(){PermsGroupID="VIP",Cost="500000"
+                },
+            };
+        }
+    }
+}
